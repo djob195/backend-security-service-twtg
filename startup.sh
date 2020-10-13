1be2120d3753aa73d476680c3e7dfe78e21735ef
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-10-13 12:52:22 -0600

    fix dockefile

--- startup.sh
+++ startup.sh
@@ -1,8 +1,2 @@
-echo "$SSH_PRIVATE" >> /root/.ssh/id_rsa
-echo "$SSH_PUBLIC" >> /root/.ssh/id_rsa.pub
-echo "$SSH_KNOWN_HOSTS" >> /root/.ssh/known_hosts
-chmod 600 /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa.pub
-npm install
 sh -c "/wait" 
-node index.js
+node /usr/src/app/index.js
