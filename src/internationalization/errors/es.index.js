eedd509b18347dbf4eccc5dd4db963733bb9bc02
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-23 15:37:30 -0600

    token fix

--- src/internationalization/errors/es.index.js
+++ src/internationalization/errors/es.index.js
@@ -2,12 +2,13 @@ module.exports = {
     "authentication":{
         "e1":"el usuario no se ha encontrado",
         "e2":"el usuario todavía no está activo en nuestro sistema",
-        "e3":"Tu usuario está activo en nuestro sistema pero debe asignarse un rol de trabajo: Comodin o fijo (En una sucursal)"
+        "e3":"Tu usuario está activo en nuestro sistema pero debe asignarse un rol de trabajo: Comodin o fijo (En una sucursal)",
+        "e4": "El token ha expirado"
     },
     "users":{
         "e1": "El usuario no debe estar asignado a un tipo comodin o fijo",
         "e2": "La compañia no existe",
         "e3": "La sucursal de la compañía no existe",
-        "e4": "El usuario tiene asignado aún entregas, lo cual no puede cambiar de tipo hasta que entrege todos"
+        "e5": "El token no existe en la base de datos"
     }
 }
