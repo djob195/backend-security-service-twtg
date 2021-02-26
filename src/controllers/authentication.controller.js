e8dd483979068c31277e374f8a4d53f4414b6918
djorellanabmrb <70344779+djorellanabmrb@users.noreply.github.com>
2021-02-26 11:38:47 -0600

    Update authentication.controller.js

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -2,6 +2,22 @@ class AuthenticationController{
     constructor({ AuthenticationService }){
         this.AuthenticationService = AuthenticationService;
     }
+    
+    
+    async deleteBikers(req,res,next){
+     try {
+            let uid = req.params.uid;
+            let data = await this.AuthenticationService.delete(uid);
+            return res.status(201).json({message:"user reseteado"}); 
+        } catch (error) {
+            console.log(error);
+            if(error.code){
+                return next({code: error.code, message: error.message});
+            }else{
+                return next({code: 500});
+            }
+        }
+    }
 
     async refresh (req, res, next){
         try {
